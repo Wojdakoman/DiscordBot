dd2ba15e01e19c531dfe3d08958f4e4e93da052a
Daniel Wojdak <logiblogi@gmail.com>
2022-12-06 19:50:09 +0100

    Add example components

--- BotFront/src/app/app.module.ts
+++ BotFront/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
